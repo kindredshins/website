8ac07522ddad8488e95165554396c0380bacdbf1
Jenna Smith <jenna@growldesign.co.uk>
2019-03-06 15:33:46 +0000

    add placeholder pages for lyrics and photos

---
+++ pages/lyrics.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '@/components/Page';
+
+const Lyrics = () => (
+  <Page title="Lyrics">
+    <p>Coming soon&hellip;</p>
+  </Page>
+);
+
+export default Lyrics;
---
+++ pages/photos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '@/components/Page';
+
+const Photos = () => (
+  <Page title="Photos">
+    <p>Coming soon&hellip;</p>
+  </Page>
+);
+
+export default Photos;
