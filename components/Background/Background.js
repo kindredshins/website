23a9d66ab0ada09a0cb10d2f8ffaf0c35e991eb1
Jenna Smith <jenna@growldesign.co.uk>
2019-03-08 10:37:58 +0000

    clean up background js

--- components/Background/Background.js
+++ components/Background/Background.js
@@ -5,62 +5,9 @@ import imagesJson from './images.json';
 
 export const Background = ({ imageSources, framesPerSecond, ...props }) => {
   const canvasRef = useRef(null);
-  const drawRef = useRef(Date.now());
+  const drawDiffRef = useRef(Date.now());
   const imagesRef = useRef([]);
   const [isLoadingImages, setIsLoadingImages] = useState(true);
-
-  function preload(image) {
-    const preloader = new Image();
-
-    // preload after other page resources
-    // have completed to avoid blocking
-    if (document.readyState === 'complete') {
-      preloader.src = image;
-    } else {
-      window.addEventListener('load', () => {
-        preloader.src = image;
-      });
-    }
-
-    return preloader;
-  }
-
-  function draw(frame = 0) {
-    if (!canvasRef.current) return;
-
-    const canvas = canvasRef.current;
-    const images = imagesRef.current;
-    const image = images[frame];
-    const interval = 1000 / framesPerSecond;
-    const context = canvas.getContext('2d', { alpha: false });
-    const nextFrame = frame + 1;
-    const hasNextFrame = nextFrame < images.length - 1;
-
-    return requestAnimationFrame(() => {
-      const now = Date.now();
-      const delta = now - drawRef.current;
-
-      if (delta > interval) {
-        drawRef.current = now - (delta % interval);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(
-          image,
-          0,
-          0,
-          image.width,
-          image.height,
-          0,
-          0,
-          canvas.width,
-          canvas.height
-        );
-
-        draw(hasNextFrame ? nextFrame : 0);
-      } else {
-        draw(frame);
-      }
-    });
-  }
 
   useEffect(() => {
     const images = imageSources.map(preload);
@@ -83,6 +30,52 @@ export const Background = ({ imageSources, framesPerSecond, ...props }) => {
       };
     }
   }, [isLoadingImages]);
+
+  function preload(image) {
+    const preloader = new Image();
+
+    // preload after other page resources
+    // have completed to avoid blocking
+    if (document.readyState === 'complete') {
+      preloader.src = image;
+    } else {
+      window.addEventListener('load', () => {
+        preloader.src = image;
+      });
+    }
+
+    return preloader;
+  }
+
+  function draw(frame = 0) {
+    const canvas = canvasRef.current;
+    const images = imagesRef.current;
+    const interval = 1000 / framesPerSecond;
+    const nextFrame = frame + 1;
+
+    if (!canvas || !images.length) return;
+
+    const image = images[frame];
+    const context = canvas.getContext('2d', { alpha: false });
+    const hasNextFrame = nextFrame < images.length - 1;
+
+    return requestAnimationFrame(() => {
+      const now = Date.now();
+      const delta = now - drawDiffRef.current;
+
+      if (delta > interval) {
+        const imageRect = [0, 0, image.width, image.height];
+        const canvasRect = [0, 0, canvas.width, canvas.height];
+        drawDiffRef.current = now - (delta % interval);
+        context.clearRect(...canvasRect);
+        context.drawImage(image, ...imageRect, ...canvasRect);
+
+        draw(hasNextFrame ? nextFrame : 0);
+      } else {
+        draw(frame);
+      }
+    });
+  }
 
   return <Canvas ref={canvasRef} background={imageSources[0]} {...props} />;
 };
