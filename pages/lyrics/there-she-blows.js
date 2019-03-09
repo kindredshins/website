bbe8d03aa5f936e8bc4cd0bba9bff84ed3ed8921
Jenna Smith <jenna@growldesign.co.uk>
2019-03-09 23:51:32 +0000

    add lyrics for there she blows

---
+++ pages/lyrics/there-she-blows.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { LyricsPage, LyricsPageTitle } from '@/components/LyricsPage';
+
+const ThereSheBlows = () => {
+  return (
+    <LyricsPage title="There She Blows">
+      <LyricsPageTitle trackId="there-she-blows">
+        There She Blows
+      </LyricsPageTitle>
+      <h3>V1</h3>
+      <p>
+        Well she asked me what’s wrong with infatuation, <br />
+        between fiends cos satisfaction,
+        <br />
+        Is all that she needs, she’ll beg and she’ll plead,
+        <br />
+        So I’ll lay her on me just occasionally and I’ll leave,
+        <br />
+        It’s easy, I’m eager to play the traitor,
+        <br />
+        So give me the best of your bad behaviour,
+        <br />
+        You’re out of my reach, not out of my taste,
+        <br />A smiles not the only thing you’ll wipe off your face, when it’s
+        done
+      </p>
+      <h3>Chorus</h3>
+      <p>
+        Well it’s ain’t my nature to call a truce,
+        <br />
+        Give you an offer that you can’t confuse,
+        <br />
+        It’s an itch so sick you don’t get to choose,
+        <br />
+        Easily lead straight to your bed,
+        <br />
+        All eyes from around the room,
+        <br />
+        But I’m the one that gets to come with you,
+        <br />
+        So I hope
+      </p>
+      <h3>V2</h3>
+      <p>
+        Well I’m safe on the stage, so I’ll see you later,
+        <br />
+        Glad that you came, I’ll repay the favour,
+        <br />
+        Pull you real close, paw at your clothes,
+        <br />
+        Cos you’re closer to the throne,
+        <br />
+        With the lower you go, don’t you know
+      </p>
+      <h3>Chorus</h3>
+      <p>
+        Well it’s ain’t my nature to call a truce,
+        <br />
+        Give you an offer that you must abuse,
+        <br />
+        It’s an itch so sick you don’t get to choose,
+        <br />
+        Easily lead straight to your bed,
+        <br />
+        All eyes from around the room,
+        <br />
+        But I’m the one that gets to come with you,
+        <br />
+        So I know
+      </p>
+    </LyricsPage>
+  );
+};
+
+export default ThereSheBlows;
