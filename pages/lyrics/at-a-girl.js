101fda273c102274839714dce94519c55192471a
Jenna Smith <jenna@growldesign.co.uk>
2019-03-10 19:07:19 +0000

    add at a girl lyrics

---
+++ pages/lyrics/at-a-girl.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { LyricsPage, LyricsPageTitle } from '@/components/LyricsPage';
+
+const AtAGirl = () => {
+  return (
+    <LyricsPage title="At A Girl">
+      <LyricsPageTitle trackId="at-a-girl">At A Girl</LyricsPageTitle>
+      <h3>V1</h3>
+      <p>
+        Please don’t, because I’m sold,
+        <br />
+        I know that I’m the culprit though but even so,
+        <br />
+        Please don’t, I’ve no control,
+        <br />I know you’re only trouble but I want you so
+      </p>
+      <h3>Chorus</h3>
+      <p>
+        Only a mind so cruel thinks a girl like you,
+        <br />
+        Is not the question to ask but the answer to,
+        <br />
+        The little things I lack, well that’s nothing new,
+        <br />
+        I won’t leave you alone,
+        <br />I won’t leave you alone he says
+      </p>
+      <h3>V2</h3>
+      <p>
+        Please don’t, because I’m sold,
+        <br />
+        I know that I’m the culprit though but even so,
+        <br />
+        Please don’t, I’m begging no,
+        <br />I know I barely know you but I want you so
+      </p>
+      <h3>Chorus</h3>
+      <p>
+        Only a mind so cruel thinks a girl like you,
+        <br />
+        Is not the question to ask but the answer to,
+        <br />
+        The little things I lack, well that’s nothing new,
+        <br />
+        I can’t leave you alone, I won’t leave you alone,
+        <br />
+        You know that skin on skin solves everything,
+        <br />
+        Even Sunday’s gift that’s only Monday’s sin,
+        <br />
+        It will hurt so bad, I can’t wait to begin,
+        <br />
+        I can’t leave you alone,
+        <br />I won’t leave you alone he says
+      </p>
+    </LyricsPage>
+  );
+};
+
+export default AtAGirl;
