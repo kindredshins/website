cd925eca6c2d88817bf6b352a27187c47da8a718
Jenna Smith <jenna@growldesign.co.uk>
2019-03-10 13:19:25 +0000

    add the spitter lyrics

---
+++ pages/lyrics/the-spitter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { LyricsPage, LyricsPageTitle } from '@/components/LyricsPage';
+
+const TheSpitter = () => (
+  <LyricsPage title="The Spitter">
+    <LyricsPageTitle trackId="the-spitter">The Spitter</LyricsPageTitle>
+    <h3>V1</h3>
+    <p>
+      And I’ve tried to please, to placate,
+      <br />
+      But it seems like I’m lost in the maze,
+      <br />
+      And I’ve tried to speak, to obey,
+      <br />
+      But you’ve left open the door to the cage,
+      <br />
+      And I’ve lied for greed, so ashamed,
+      <br />
+      Can’t believe that I’m fanning the flames,
+      <br />
+      And I’ve tried to be what you say,
+      <br />
+      It makes no difference to me anyway
+    </p>
+    <h3>Chorus</h3>
+    <p>
+      And still in I lean for a little kiss,
+      <br />
+      And still now I plead for a little bit,
+      <br />
+      I’m killed by the need of another rift,
+      <br />
+      And still in I lean for a little kiss
+    </p>
+  </LyricsPage>
+);
+
+export default TheSpitter;
