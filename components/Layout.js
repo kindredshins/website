f699035718eb46c528b3d58456493fa4921f30a1
Jenna Smith <jenna@growldesign.co.uk>
2019-03-13 16:18:56 +0000

    allow search engines to crawl

--- components/Layout.js
+++ components/Layout.js
@@ -21,7 +21,6 @@ export const Layout = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Kindred Shins</title>
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="robots" content="noindex, nofollow" />
         <link
           rel="apple-touch-icon"
           sizes="57x57"
